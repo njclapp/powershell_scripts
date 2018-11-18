ed25e57f5af1f389da548340287893e1ea7ea68d
Nate Clapp <njclapp1@gmail.com>
2018-11-17 19:36:41 -0600

    Update Invoke-Exploration.ps1 creds issue

--- Invoke-Exploration.ps1
+++ Invoke-Exploration.ps1
@@ -1,6 +1,4 @@
-﻿#Requires -runasadministrator
-
-<#
+﻿<#
 .SYNOPSIS
 This script maps out an active directory environment. Useful for use in a new environment.
 
@@ -8,18 +6,20 @@ This script maps out an active directory environment. Useful for use in a new en
 ./Invoke-Exploration.ps1
 #>
 
+$creds = Get-Credential
+
 $output = New-Object System.Object
 
-$output | Add-Member -MemberType NoteProperty -Name User_Count -Value $(Get-ADUser -Filter * | measure).count
-$output | Add-Member -MemberType NoteProperty -Name Computer_Count -Value $(Get-ADComputer -Filter * | measure).Count
-$output | Add-Member -MemberType NoteProperty -Name DC_Count -Value $(Get-ADDomainController -Filter *).count
-$output | Add-Member -MemberType NoteProperty -Name ReplicaDC -Value $(Get-ADDomain).ReplicaDirectoryServers
-$output | Add-Member -MemberType NoteProperty -Name Group_Count -Value $(Get-ADGroup -Filter * | where {$_.GroupScope -like "Global"}).count
-$output | Add-Member -MemberType NoteProperty -Name OU_Count -Value $(Get-ADOrganizationalUnit -Filter *).count
+$output | Add-Member -MemberType NoteProperty -Name User_Count -Value $(Get-ADUser -Filter * -Credential $creds | measure).count
+$output | Add-Member -MemberType NoteProperty -Name Computer_Count -Value $(Get-ADComputer -Filter * -Credential $creds| measure).Count
+$output | Add-Member -MemberType NoteProperty -Name DC_Count -Value $(Get-ADDomainController -Filter * -Credential $creds).count
+$output | Add-Member -MemberType NoteProperty -Name ReplicaDC -Value $(Get-ADDomain -Credential $creds).ReplicaDirectoryServers
+$output | Add-Member -MemberType NoteProperty -Name Group_Count -Value $(Get-ADGroup -Filter * -Credential $creds| where {$_.GroupScope -like "Global"}).count
+$output | Add-Member -MemberType NoteProperty -Name OU_Count -Value $(Get-ADOrganizationalUnit -Filter * -Credential $creds).count
 
-$DCs = Get-ADDomainController -Filter * | ft Name,IPv4Address,Forest,Domain,OperatingSystem
-$Groups = Get-ADGroup -Filter * | where {$_.GroupScope -like "Global"} | ft Name,GroupScope,GroupCategory
-$OUs = Get-ADOrganizationalUnit -Filter * | ft Name,ObjectClass
+$DCs = Get-ADDomainController -Filter * -Credential $creds | ft Name,IPv4Address,Forest,Domain,OperatingSystem
+$Groups = Get-ADGroup -Filter * -Credential $creds | where {$_.GroupScope -like "Global"} | ft Name,GroupScope,GroupCategory
+$OUs = Get-ADOrganizationalUnit -Filter * -Credential $creds | ft Name,ObjectClass
 
 
 $output | fl
